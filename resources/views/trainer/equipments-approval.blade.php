1546fff8c9575997dd816b3babd894ce4d1f632d
mirza-sync <mirzasyahmi123@gmail.com>
2021-08-11 23:28:29 +0800

    Fix table in card missing padding

--- resources/views/trainer/equipments-approval.blade.php
+++ resources/views/trainer/equipments-approval.blade.php
@@ -22,139 +22,144 @@
                 <div class="col-lg-4">
                     <div class="page-header-breadcrumb">
                        <ul class="breadcrumb-title">
-                        <li class="breadcrumb-item">
-                            <a href="/home">
-                                <i class="icofont icofont-home"></i>
-                            </a>
-                        </li>
-                        <li class="breadcrumb-item"><a href="">Application Approval</a>
-                        </li>
-                    </ul>
+                            <li class="breadcrumb-item">
+                                <a href="/home">
+                                    <i class="icofont icofont-home"></i>
+                                </a>
+                            </li>
+                            <li class="breadcrumb-item"><a href="">Application Approval</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- Page-header end -->
+        <!-- Page-header end -->
 
-    <div class="card" background-color="#a69d82">
-        <div class="card-header">
-            <h4>List of Application</h4>
-            <div class="table-responsive">
-    
-            <div class="card-header-right">    <ul class="list-unstyled card-option">        <li><i class="icofont icofont-simple-left "></i></li>        <li><i class="icofont icofont-maximize full-card"></i></li>        <li><i class="icofont icofont-minus minimize-card"></i></li>        <li><i class="icofont icofont-refresh reload-card"></i></li>        <li><i class="icofont icofont-error close-card"></i></li>    </ul></div>
-        </div>
-        <div class="card-block table-border-style">
+        <div class="card" background-color="#a69d82">
+            <div class="card-header">
+                <h4>List of Application</h4>
+                <div class="table-responsive">
+        
+                    <div class="card-header-right">    <ul class="list-unstyled card-option">        <li><i class="icofont icofont-simple-left "></i></li>        <li><i class="icofont icofont-maximize full-card"></i></li>        <li><i class="icofont icofont-minus minimize-card"></i></li>        <li><i class="icofont icofont-refresh reload-card"></i></li>        <li><i class="icofont icofont-error close-card"></i></li>    </ul></div>
+                </div>
+            </div>
+            <div class="card-block table-border-style px-3 pb-3">
                 <table class="table">
                     <thead class="bg-primary text-white">
                         <tr>
-                        <th>Num</th>
-                        <th>Military Number</th>
-                        <th>Rank</th>
-                        <th>Full Name</th>
-                        <th>Equipments</th>
-                        <th>Date Application</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($cadets as $cadet)
-                        <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td>{{$cadet->cadetID}}</td>
-                            <td>{{$cadet->cadetRank}}</td>
-                            <td>{{$cadet->cadetName}}</td>
-                            <td>
-                                <ul>
-                                    @foreach ($cadet->equipments as $equipment)
-                                        <li style="list-style-type: disc;">{{$equipment->equipName}}</li>
-                                    @endforeach
-                                </ul>
-                            </td>
-                            <td>
-                                @php
-                                    $created = $cadet->equipments;
-                                    echo $created[0]->pivot->created_at;
-                                @endphp
-                            </td>
-                            <td>
-                                @php
-                                    $application = $cadet->equipments;
-                                    echo $application[0]->pivot->status;
-                                @endphp
-                            </td>
-                            <td>
-                                {{-- <button type="submit" class="btn btn-primary waves-effect waves-light">Submit</button> --}}
-                                <a href="/equipments-approve/{{$cadet->id}}" class="btn btn-primary waves-effect waves-light">Approve</a>
-                            </td>
+                            <th>Num</th>
+                            <th>Military Number</th>
+                            <th>Rank</th>
+                            <th>Full Name</th>
+                            <th>Equipments</th>
+                            <th>Date Application</th>
+                            <th>Status</th>
+                            <th>Action</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach($cadets as $cadet)
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$cadet->cadetID}}</td>
+                                <td>{{$cadet->cadetRank}}</td>
+                                <td>{{$cadet->cadetName}}</td>
+                                <td>
+                                    <ul>
+                                        @foreach ($cadet->equipments as $equipment)
+                                            <li style="list-style-type: disc; white-space: break-spaces">{{$equipment->equipName}}</li>
+                                        @endforeach
+                                    </ul>
+                                </td>
+                                <td>
+                                    @php
+                                        $created = $cadet->equipments;
+                                        echo $created[0]->pivot->created_at;
+                                    @endphp
+                                </td>
+                                <td>
+                                    @php
+                                        $application = $cadet->equipments;
+                                        echo $application[0]->pivot->status;
+                                    @endphp
+                                </td>
+                                <td>
+                                    {{-- <button type="submit" class="btn btn-primary waves-effect waves-light">Submit</button> --}}
+                                    <a href="/equipments-approve/{{$cadet->id}}" class="btn btn-primary waves-effect waves-light">Approve</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-</div>
-<div>
-</div>
-<div class="card" background-color="#a69d82">
-    <div class="card-header">
-        <h4>Recently Approved</h4>
-        <div class="table-responsive">
+        {{-- end card --}}
+        
+        <div class="card" background-color="#a69d82">
+            <div class="card-header">
+                <h4>Recently Approved</h4>
+                <div class="table-responsive">
 
-        <div class="card-header-right">    <ul class="list-unstyled card-option">        <li><i class="icofont icofont-simple-left "></i></li>        <li><i class="icofont icofont-maximize full-card"></i></li>        <li><i class="icofont icofont-minus minimize-card"></i></li>        <li><i class="icofont icofont-refresh reload-card"></i></li>        <li><i class="icofont icofont-error close-card"></i></li>    </ul></div>
-    </div>
-    <div class="card-block table-border-style">
-            <table class="table">
-                <thead class="bg-primary text-white">
-                    <tr>
-                        <th>Num</th>
-                        <th>Military Number</th>
-                        <th>Rank</th>
-                        <th>Full Name</th>
-                        <th>Equipments</th>
-                        <th>Date Application</th>
-                        <th>Date Approved</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($approvedList as $approved)
+                    <div class="card-header-right">    <ul class="list-unstyled card-option">        <li><i class="icofont icofont-simple-left "></i></li>        <li><i class="icofont icofont-maximize full-card"></i></li>        <li><i class="icofont icofont-minus minimize-card"></i></li>        <li><i class="icofont icofont-refresh reload-card"></i></li>        <li><i class="icofont icofont-error close-card"></i></li>    </ul></div>
+                </div>
+            </div>
+            <div class="card-block table-border-style px-3 pb-3">
+                <table class="table">
+                    <thead class="bg-primary text-white">
                         <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td>{{$approved->cadetID}}</td>
-                            <td>{{$approved->cadetRank}}</td>
-                            <td>{{$approved->cadetName}}</td>
-                            <td>
-                                <ul>
-                                    @foreach ($approved->equipments as $equipment)
-                                        <li style="list-style-type: disc;">{{$equipment->equipName}}</li>
-                                    @endforeach
-                                </ul>
-                            </td>
-                            <td>
-                                @php
-                                    $created = $approved->equipments;
-                                    echo $created[0]->pivot->created_at;
-                                @endphp
-                            </td>
-                            <td>
-                                @php
-                                    $application = $approved->equipments;
-                                    echo $application[0]->pivot->updated_at;
-                                @endphp
-                            </td>
-                            <td>
-                                @php
-                                    $application = $approved->equipments;
-                                    echo $application[0]->pivot->status;
-                                @endphp
-                            </td>
+                            <th>Num</th>
+                            <th>Military Number</th>
+                            <th>Rank</th>
+                            <th>Full Name</th>
+                            <th>Equipments</th>
+                            <th>Date Application</th>
+                            <th>Date Approved</th>
+                            <th>Status</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach($approvedList as $approved)
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$approved->cadetID}}</td>
+                                <td>{{$approved->cadetRank}}</td>
+                                <td>{{$approved->cadetName}}</td>
+                                <td>
+                                    <ul>
+                                        @foreach ($approved->equipments as $equipment)
+                                            <li style="list-style-type: disc; white-space: break-spaces">{{$equipment->equipName}}</li>
+                                        @endforeach
+                                    </ul>
+                                </td>
+                                <td>
+                                    @php
+                                        $created = $approved->equipments;
+                                        echo $created[0]->pivot->created_at;
+                                    @endphp
+                                </td>
+                                <td>
+                                    @php
+                                        $application = $approved->equipments;
+                                        echo $application[0]->pivot->updated_at;
+                                    @endphp
+                                </td>
+                                <td>
+                                    @php
+                                        $application = $approved->equipments;
+                                        echo $application[0]->pivot->status;
+                                    @endphp
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
+        {{-- End card --}}
     </div>
+    {{-- End page wrapper --}}
 </div>
+{{-- End main page --}}
 
 @endsection
--- resources/views/trainer/equipments-return.blade.php
+++ resources/views/trainer/equipments-return.blade.php
@@ -22,136 +22,137 @@
                 <div class="col-lg-4">
                     <div class="page-header-breadcrumb">
                        <ul class="breadcrumb-title">
-                        <li class="breadcrumb-item">
-                            <a href="/home">
-                                <i class="icofont icofont-home"></i>
-                            </a>
-                        </li>
-                        <li class="breadcrumb-item"><a href="">Equipment Return</a>
-                        </li>
-                    </ul>
+                            <li class="breadcrumb-item">
+                                <a href="/home">
+                                    <i class="icofont icofont-home"></i>
+                                </a>
+                            </li>
+                            <li class="breadcrumb-item"><a href="">Equipment Return</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- Page-header end -->
+        <!-- Page-header end -->
 
-<div class="card" background-color="#a69d82">
-    <div class="card-header">
-        <h4>List of Lending</h4>
-        <div class="table-responsive">
+        <div class="card" background-color="#a69d82">
+            <div class="card-header">
+                <h4>List of Lending</h4>
+                <div class="table-responsive">
 
-        <div class="card-header-right">    <ul class="list-unstyled card-option">        <li><i class="icofont icofont-simple-left "></i></li>        <li><i class="icofont icofont-maximize full-card"></i></li>        <li><i class="icofont icofont-minus minimize-card"></i></li>        <li><i class="icofont icofont-refresh reload-card"></i></li>        <li><i class="icofont icofont-error close-card"></i></li>    </ul></div>
-    </div>
-    <div class="card-block table-border-style">
-            <table class="table">
-                <thead class="bg-primary text-white">
-                    <tr>
-                        <th>Num</th>
-                        <th>Military Number</th>
-                        <th>Rank</th>
-                        <th>Full Name</th>
-                        <th>Equipments</th>
-                        <th>Date Approved</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($cadetApproved as $cadet)
+                    <div class="card-header-right">    <ul class="list-unstyled card-option">        <li><i class="icofont icofont-simple-left "></i></li>        <li><i class="icofont icofont-maximize full-card"></i></li>        <li><i class="icofont icofont-minus minimize-card"></i></li>        <li><i class="icofont icofont-refresh reload-card"></i></li>        <li><i class="icofont icofont-error close-card"></i></li>    </ul></div>
+                </div>
+            </div>
+            <div class="card-block table-border-style px-3 pb-3">
+                <table class="table">
+                    <thead class="bg-primary text-white">
                         <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td>{{$cadet->cadetID}}</td>
-                            <td>{{$cadet->cadetRank}}</td>
-                            <td>{{$cadet->cadetName}}</td>
-                            <td>
-                                <ul>
-                                    @foreach ($cadet->equipments as $equipment)
-                                        <li style="list-style-type: disc;">{{$equipment->equipName}}</li>
-                                    @endforeach
-                                </ul>
-                            </td>
-                            <td>
-                                @php
-                                    $application = $cadet->equipments;
-                                    echo $application[0]->pivot->updated_at;
-                                @endphp
-                            </td>
-                            <td>
-                                @php
-                                    $application = $cadet->equipments;
-                                    echo $application[0]->pivot->status;
-                                @endphp
-                            </td>
-                            <td>
-                                <a href="/equipments-return/{{$cadet->id}}" class="btn btn-primary waves-effect waves-light">Return</a>
-                            </td>
+                            <th>Num</th>
+                            <th>Military Number</th>
+                            <th>Rank</th>
+                            <th>Full Name</th>
+                            <th>Equipments</th>
+                            <th>Date Approved</th>
+                            <th>Status</th>
+                            <th>Action</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach($cadetApproved as $cadet)
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$cadet->cadetID}}</td>
+                                <td>{{$cadet->cadetRank}}</td>
+                                <td>{{$cadet->cadetName}}</td>
+                                <td>
+                                    <ul>
+                                        @foreach ($cadet->equipments as $equipment)
+                                            <li style="list-style-type: disc; white-space: break-spaces;">{{$equipment->equipName}}</li>
+                                        @endforeach
+                                    </ul>
+                                </td>
+                                <td>
+                                    @php
+                                        $application = $cadet->equipments;
+                                        echo $application[0]->pivot->updated_at;
+                                    @endphp
+                                </td>
+                                <td>
+                                    @php
+                                        $application = $cadet->equipments;
+                                        echo $application[0]->pivot->status;
+                                    @endphp
+                                </td>
+                                <td>
+                                    <a href="/equipments-return/{{$cadet->id}}" class="btn btn-primary waves-effect waves-light">Return</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-</div>
-<div>
-</div>
-<div class="card" background-color="#a69d82">
-    <div class="card-header">
-        <h4>Recently Return</h4>
-        <div class="table-responsive">
 
-        <div class="card-header-right">    <ul class="list-unstyled card-option">        <li><i class="icofont icofont-simple-left "></i></li>        <li><i class="icofont icofont-maximize full-card"></i></li>        <li><i class="icofont icofont-minus minimize-card"></i></li>        <li><i class="icofont icofont-refresh reload-card"></i></li>        <li><i class="icofont icofont-error close-card"></i></li>    </ul></div>
-    </div>
-    <div class="card-block table-border-style">
-            <table class="table">
-                <thead class="bg-primary text-white">
-                    <tr>
-                        <th>Num</th>
-                        <th>Military Number</th>
-                        <th>Rank</th>
-                        <th>Full Name</th>
-                        <th>Items</th>
-                        <th>Date Application</th>
-                        <th>Date Returned</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($cadetReturned as $cadet)
+        <div class="card" background-color="#a69d82">
+            <div class="card-header">
+                <h4>Recently Return</h4>
+                <div class="table-responsive">
+
+                    <div class="card-header-right">    <ul class="list-unstyled card-option">        <li><i class="icofont icofont-simple-left "></i></li>        <li><i class="icofont icofont-maximize full-card"></i></li>        <li><i class="icofont icofont-minus minimize-card"></i></li>        <li><i class="icofont icofont-refresh reload-card"></i></li>        <li><i class="icofont icofont-error close-card"></i></li>    </ul></div>
+                </div>
+            </div>
+            <div class="card-block table-border-style px-3 pb-3">
+                <table class="table">
+                    <thead class="bg-primary text-white">
                         <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td>{{$cadet->cadetID}}</td>
-                            <td>{{$cadet->cadetRank}}</td>
-                            <td>{{$cadet->cadetName}}</td>
-                            <td>
-                                <ul>
-                                    @foreach ($cadet->equipments as $equipment)
-                                        <li style="list-style-type: disc;">{{$equipment->equipName}}</li>
-                                    @endforeach
-                                </ul>
-                            </td>
-                            <td>
-                                @php
-                                    $application = $cadet->equipments;
-                                    echo $application[0]->pivot->created_at;
-                                @endphp
-                            </td>
-                            <td>
-                                @php
-                                    $application = $cadet->equipments;
-                                    echo $application[0]->pivot->updated_at;
-                                @endphp
-                            </td>
-                            <td>
-                                @php
-                                    $application = $cadet->equipments;
-                                    echo $application[0]->pivot->status;
-                                @endphp
-                            </td>
+                            <th>Num</th>
+                            <th>Military Number</th>
+                            <th>Rank</th>
+                            <th>Full Name</th>
+                            <th>Items</th>
+                            <th>Date Application</th>
+                            <th>Date Returned</th>
+                            <th>Status</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach($cadetReturned as $cadet)
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$cadet->cadetID}}</td>
+                                <td>{{$cadet->cadetRank}}</td>
+                                <td>{{$cadet->cadetName}}</td>
+                                <td>
+                                    <ul>
+                                        @foreach ($cadet->equipments as $equipment)
+                                            <li style="list-style-type: disc; white-space: break-spaces;">{{$equipment->equipName}}</li>
+                                        @endforeach
+                                    </ul>
+                                </td>
+                                <td>
+                                    @php
+                                        $application = $cadet->equipments;
+                                        echo $application[0]->pivot->created_at;
+                                    @endphp
+                                </td>
+                                <td>
+                                    @php
+                                        $application = $cadet->equipments;
+                                        echo $application[0]->pivot->updated_at;
+                                    @endphp
+                                </td>
+                                <td>
+                                    @php
+                                        $application = $cadet->equipments;
+                                        echo $application[0]->pivot->status;
+                                    @endphp
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
